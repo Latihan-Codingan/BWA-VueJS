0e0d76d0376558bd63f0140abeac6b85ba146dff
Harun1804 <harun.arrasyid1804@gmail.com>
2020-08-24 13:59:34 +0700

    Instalasi Router,Bootstrap

---
+++ vuegogo/src/views/profile.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="profile">
+    <!-- <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <h1>This is a Profile page</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+
+export default {
+  name: 'profile',
+  components: {
+    //HelloWorld
+  }
+}
+</script>
