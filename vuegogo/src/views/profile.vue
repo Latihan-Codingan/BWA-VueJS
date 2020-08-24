3044db305f7804bff46b465b5b9068b9adddd3eb
Harun1804 <harun.arrasyid1804@gmail.com>
2020-08-24 14:18:20 +0700

    Data Json

--- vuegogo/src/views/profile.vue
+++ vuegogo/src/views/profile.vue
@@ -2,19 +2,42 @@
   <div class="profile">
     <!-- <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <h1>This is a Profile page</h1>
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-6">
+          <div class="media text-left">
+            <img class="mr-3 user-pict" src="../assets/kiva.jpg" alt="Logo">
+            <div class="media-body">
+              <h5 class="mt-0">{{ firstname }} {{ lastname }}</h5>
+              <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
-import 'bootstrap/dist/css/bootstrap.css'
+  // @ is an alias to /src
+  import 'bootstrap/dist/css/bootstrap.css'
 
-export default {
-  name: 'profile',
-  components: {
-    //HelloWorld
+  export default {
+    name: 'profile',
+    data: function(){
+      return {
+        firstname : 'Harun',
+        lastname : 'Ar - Rasyid'
+      }
+    },
+    component:{}
   }
-}
 </script>
+
+<style scoped>
+.user-pict {
+  border-radius: 200%;
+  height: 80px;
+  width: 80px;
+}
+</style>
