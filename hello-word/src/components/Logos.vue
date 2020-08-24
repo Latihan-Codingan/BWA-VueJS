ee1c92b7028918db06f2259f3bddbe0e69775c18
Harun1804 <harun.arrasyid1804@gmail.com>
2020-08-24 11:51:51 +0700

    Tutorial BWA Bagian Start Project Judul Instalasai Dev Tools

---
+++ hello-word/src/components/Logos.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="logos-wrapper">
+        <h2 class="title-header">Find Best Logo</h2>
+        <div class="item-logos">
+            <img class="logo" src="../assets/1.jpeg" alt="Sonic 1">
+        </div>
+        <div class="item-logos">
+            <img class="logo" src="../assets/2.jpeg" alt="Sonic 2">
+        </div>
+        <div class="item-logos">
+            <img class="logo" src="../assets/3.jpeg" alt="Sonic 3">
+        </div>
+    </div>
+</template>
+    
+<script>
+export default {
+    name : 'Logos'
+}
+</script>
+
+<style scoped>
+.title-header {
+    margin-bottom: 80px;
+    text-align: left;
+}
+.logos-wrapper {
+    width: 62%;
+    overflow: hidden;
+    margin: auto;
+    
+}
+
+.item-logos {
+    width: 150px;
+    height: 150px;
+    float: left;
+    margin-right: 70px;
+    margin-bottom: 70px;
+}
+
+.item-logos .logo{
+    width: 150px;
+    height: 150px;
+}
+</style>
