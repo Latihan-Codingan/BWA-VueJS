bd66a19d3416e488a41185d8d7ae8ca3dba7aac5
Harun1804 <harun.arrasyid1804@gmail.com>
2020-08-25 14:21:55 +0700

    Props

---
+++ latihan-data/src/components/ItemTugas.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div class="form-check">
+            <input class="form-check-input" onclik="" type="checkbox" id="defaultCheck1">
+            <label class="form-check-label item" v-bind:class="{'item-completed':tugasSaya.isDone}"
+                for="defaultCheck1">{{ tugasSaya.namaTugas }}</label>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ItemTugas',
+        props:["tugasSaya"]
+    }
+</script>
+
+<style scoped>
+
+</style>
